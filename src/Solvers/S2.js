fa9642480a9f9e9d25f1fb909625cb6cdb10c404
pmanlig <patrik@manlig.org>
2020-12-12 03:54:13 +0100

    Day 3

--- src/Solvers/S2.js
+++ src/Solvers/S2.js
@@ -2,8 +2,20 @@
 import Solver from './Solver';
 
 export class S2a extends Solver {
+	dividers(l) {
+		for (let i = 0; i < l.length - 1; i++) {
+			for (let j = i + 1; j < l.length; j++) {
+				if (l[i] % l[j] === 0) return l[i] / l[j];
+				if (l[j] % l[i] === 0) return l[j] / l[i];
+			}
+		}
+	}
+
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		input = input.split('\n').map(l => l.split('\t').map(n => parseInt(n, 10)));
+		let checksum = input.map(l => Math.max(...l) - Math.min(...l)).reduce((a, b) => a + b, 0);
+		let checksum2 = input.map(l => this.dividers(l)).reduce((a, b) => a + b, 0);
+		this.setState({ solution: `Checksum(1): ${checksum}\nChecksum(2): ${checksum2}` });
 	}
 }
 
--- src/Solvers/S3.js
+++ src/Solvers/S3.js
@@ -1,9 +1,69 @@
 // import React from 'react';
 import Solver from './Solver';
 
+class Map {
+	constructor() {
+		this.data = [];
+	}
+
+	y(y) {
+		if (this.data[y] === undefined) this.data[y] = [];
+		return this.data[y];
+	}
+
+	put(x, y, val) {
+		this.y(y)[x] = val;
+	}
+
+	get(x, y) {
+		return this.y(y)[x] || 0;
+	}
+
+	sum(x, y) {
+		return this.get(x + 1, y + 1) +
+			this.get(x + 1, y) +
+			this.get(x + 1, y - 1) +
+			this.get(x, y + 1) +
+			this.get(x, y - 1) +
+			this.get(x - 1, y + 1) +
+			this.get(x - 1, y) +
+			this.get(x - 1, y - 1);
+	}
+}
+
 export class S3a extends Solver {
+	step({ x, y, r }) {
+		if (x === r && y === r) { return { x: x + 1, y: y, r: r + 1 }; }
+		if (x === r && y > -r) { return { x: x, y: y - 1, r: r }; }
+		if (y === -r && x > -r) { return { x: x - 1, y: y, r: r }; }
+		if (x === -r && y < r) { return { x: x, y: y + 1, r: r }; }
+		if (y === r && x < r) { return { x: x + 1, y: y, r: r }; }
+	}
+
+	cell(n) {
+		let pos = 1;
+		let coord = { x: 0, y: 0, r: 0 }
+		while (pos++ < n) {
+			coord = this.step(coord);
+		}
+		return coord;
+	}
+
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		input = parseInt(input, 10);
+		let map = new Map();
+		map.put(0, 0, 1);
+		let pos = 1;
+		let coord = { x: 0, y: 0, r: 0 }
+		let val = 1;
+		while (pos++ < input) {
+			coord = this.step(coord);
+			if (val <= input) {
+				val = map.sum(coord.x, coord.y);
+				map.put(coord.x, coord.y, val);
+			}
+		}
+		this.setState({ solution: `Distance: ${Math.abs(coord.x) + Math.abs(coord.y)}\nValue: ${val}` });
 	}
 }
 
