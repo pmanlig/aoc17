a8549f7f1130d9207baf51263e72c1e50f72273b
pmanlig <patrik@manlig.org>
2020-12-15 07:45:30 +0100

    Day 13

--- src/Solvers/S12.js
+++ src/Solvers/S12.js
@@ -2,8 +2,43 @@
 import Solver from './Solver';
 
 export class S12a extends Solver {
+	addLinks(input, links, nums) {
+		input = input.match(/^(\d+) <-> (.*)/);
+		let from = parseInt(input[1], 10);
+		if (links[from] === undefined) { links[from] = []; }
+		if (!nums.includes(from)) { nums.push(from); }
+		input[2].match(/(\d+)/g).forEach(t => {
+			let to = parseInt(t, 10);
+			if (links[to] === undefined) { links[to] = []; }
+			if (!nums.includes(to)) { nums.push(to); }
+			if (!links[from].includes(to)) { links[from].push(to); }
+			if (!links[to].includes(from)) { links[to].push(from); }
+		});
+	}
+
+	createGroup(n, links) {
+		let connected = [n];
+		let i = 0;
+		while (i < connected.length) {
+			links[connected[i++]].forEach(j => { if (!connected.includes(j)) { connected.push(j); } });
+		}
+		return connected;
+	}
+
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		// input = "0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5";
+		input = input.split('\n');
+		let links = [];
+		let numbers = [];
+		input.forEach(l => this.addLinks(l, links, numbers));
+		let connected = this.createGroup(0, links);
+		let groups = [];
+		while (numbers.length > 0) {
+			let g = this.createGroup(numbers.shift(), links);
+			groups.push(g);
+			numbers = numbers.filter(n => !g.includes(n));
+		}
+		this.setState({ solution: `# connected programs: ${connected.length}\n# groups: ${groups.length}` });
 	}
 }
 
--- src/Solvers/S13.js
+++ src/Solvers/S13.js
@@ -1,9 +1,33 @@
 //import React from 'react';
 import Solver from './Solver';
 
+class Scanner {
+	constructor(depth, range) {
+		this.depth = depth;
+		this.range = range;
+	}
+
+	period() { return 2 * (this.range - 1); }
+	caught(t) { return (t % this.period()) === 0; }
+	severity() { return this.depth * this.range; }
+}
+
 export class S13a extends Solver {
+	caught(scanners, delay) {
+		return scanners.filter(s => s.caught(s.depth + delay));
+	}
+
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		// input = "0: 3\n1: 2\n4: 4\n6: 4";
+		let scanners = input.split('\n').map(s => s.match(/(\d+)/g)).map(s => new Scanner(parseInt(s[0], 10), parseInt(s[1], 10)));
+		let totalSeverity = scanners.filter(s => s.caught(s.depth)).map(s => s.severity()).reduce((a, b) => a + b, 0);
+		let delay = 0;
+		let caught = this.caught(scanners, delay);
+		while (caught.length > 0) {
+			delay++;
+			caught = this.caught(scanners, delay);
+		}
+		this.setState({ solution: `Total severity: ${totalSeverity}\nMinimum delay: ${delay}` });
 	}
 }
 
