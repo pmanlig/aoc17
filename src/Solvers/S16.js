8f6692c41f0fc370d580610775239b9a69f72ec4
pmanlig <patrik@manlig.org>
2020-12-20 00:41:19 +0100

    Day 17

--- src/Solvers/S16.js
+++ src/Solvers/S16.js
@@ -2,8 +2,39 @@
 import Solver from './Solver';
 
 export class S16a extends Solver {
+	reorder(prg, i) {
+		let m = i.match(/^s(\d+)/);
+		if (m) {
+			m[1] = parseInt(m[1], 10);
+			return prg.slice(prg.length - m[1]).concat(prg.slice(0, prg.length - m[1]));
+		}
+		m = i.match(/^x(\d+)\/(\d+)/);
+		if (m) {
+			m[1] = parseInt(m[1], 10);
+			m[2] = parseInt(m[2], 10);
+			let c = prg[m[1]];
+			prg[m[1]] = prg[m[2]];
+			prg[m[2]] = c;
+			return prg;
+		}
+		m = i.match(/^p([a-p])\/([a-p])/);
+		if (m) {
+			return prg.map(p => p === m[1] ? m[2] : (p === m[2] ? m[1] : p));
+		}
+		throw new Error(`Error: ${i}`);
+	}
+
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		// input = "s3";
+		input = input.split(',');
+		let prg = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
+		let states = [];
+		let update = i => prg = this.reorder(prg, i);
+		while (!states.includes(prg.join(''))) {
+			states.push(prg.join(''));
+			input.forEach(update);
+		}
+		this.setState({ solution: `Instructions: ${input.length}\nFinal order: ${states[1]}\nBillionth state: ${states[1000000000 % states.length]}` });
 	}
 }
 
--- src/Solvers/S17.js
+++ src/Solvers/S17.js
@@ -3,7 +3,22 @@ import Solver from './Solver';
 
 export class S17a extends Solver {
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		// input = "3";
+		input = parseInt(input, 10);
+		let pos = 0, buf = [0];
+		while (buf.length < 2018) {
+			pos = (pos + input) % buf.length;
+			buf.splice(++pos, 0, buf.length);
+			// console.log(`[${buf.join(", ")}] (${pos})`);
+		}
+		let val1 = buf[(pos + 1) % buf.length];
+		let step = 1, val2 = 1;
+		pos = 1;
+		while (step++ < 50000000) {
+			pos = (pos + input) % step;
+			if (pos++ === 0) { val2 = step; }
+		}
+		this.setState({ solution: `Value: ${val1}\nAdjusted value: ${val2}` });
 	}
 }
 
